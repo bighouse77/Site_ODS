9a44e6f9c7f22c3d249adc1bb38f1dac2147e576
LuizScalixto <85201534+LuizScalixto@users.noreply.github.com>
2021-06-07 20:44:32 -0300

    Update form.php

--- site/form.php
+++ site/form.php
@@ -1,7 +1,10 @@
 <html>
     <body>
-        <form method="get" action".php">
-          <p>Nome:<input type="text"name="nome</p>
+        <form method="post" action="form.php">
+          <p>Nome:<input type="text"name="nome"></p>
+          <p>CPF:<input type="text"name="CPF"></p>
+          <p>Email:<input type="text"name="Email"></p>
+          <p>Senha:<input type="password"name="Senha"></p>
           <p><input type="submit" value="Enviar"></p>
         </form>
 
