c04c789e638674694ab4fdb626568033ae6bcd6e
LuizScalixto <85201534+LuizScalixto@users.noreply.github.com>
2021-06-07 20:42:20 -0300

    <p>Email:<input type="text"name="Email"></p>

---
+++ site/form.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <form method="get" action".php">
+          <p>Nome:<input type="text"name="nome</p>
+          <p><input type="submit" value="Enviar"></p>
+        </form>
+
