4478b3fcef3465ac21c637a6156d218e9ee27854
Mauricio Casagrande <mauricio.araras@hotmail.com>
2021-06-14 22:18:28 -0300

    att

--- site/frontend/animacao/animacao.js
+++ site/frontend/animacao/animacao.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             $("#grafico").show("slow");
             
         }, function () {
-            $("#grafico").hide("slow");
+            $("#grafico").stop(true, true).hide("slow");
         }
     );
 
@@ -16,7 +16,7 @@ $(document).ready(function () {
         $("#grafico").show("slow");
         
     }, function () {
-        $("#grafico").hide("slow");
+        $("#grafico").stop(true, true).hide("slow");
     }
 );
 
@@ -25,7 +25,7 @@ $(document).ready(function () {
         $("#grafico").show("slow");
         
     }, function () {
-        $("#grafico").hide("slow");
+        $("#grafico").stop(true, true).hide("slow");
     }
     );
 });
