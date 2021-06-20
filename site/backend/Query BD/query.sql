b6660072a887f73fc0fcfaad3084ced1633f1271
Mauricio Casagrande <mauricio.araras@hotmail.com>
2021-06-20 17:02:22 -0300

    att

--- site/backend/Query BD/query.sql
+++ site/backend/Query BD/query.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS site_ods;
 USE site_ods;
 
 CREATE TABLE IF NOT EXISTS cadastro (
-    id INT,
-    Nome_Razão VARCHAR(30),
-    CPF_CNPJ VARCHAR(30) NOT NULL,
+
+    Nome VARCHAR(30),
+    CPF_CNPJ VARCHAR(30),
     Email VARCHAR(50),
     Senha VARCHAR(50),
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (CPF_CNPJ)
 );
--- site/backend/cadastro.php
+++ site/backend/cadastro.php
@@ -1,5 +1,25 @@
 <?php
+    
+    //CONEXÃO COM O BANCO DE DADOS
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('DB_NAME', 'site_ods');
 
-    echo "TA CERTO";
+    $banco = new PDO('mysql:host='.HOST.'; dbname='.DB_NAME,USER,PASSWORD);
+    //*****************************/
+    
+    $nome = $_POST['nome'];  
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $cpf = $_POST['cpf'];
+
+    $cadastro = array($nome, $cpf, $email, $senha);
+
+    $gravar = $banco->prepare(
+        "INSERT INTO cadastro (Nome, CPF_CNPJ, Email, Senha)
+        values (?, ?, ?)"
+    );
+    $gravar->execute($cadastro);
 
 ?>
